1060c818c9e843a0b326c5ee72cb1ea6de0c941c
lgutie16 <lindaandgod@gmail.com>
2019-02-13 17:53:17 -0500

    grid layout - basic setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header Event-name">
+          Advance Fullstack REACT/NODE App & AWS deployment{' '}
         </header>
+        <section className="Event-three-days">
+          <section className="Speaker">
+            <div className="Speaker-image--Container">
+              <img
+                className="Speaker-image"
+                src="/public/speakers/jhon-carlo.jpg"
+                alt="speaker photograpy"
+              />
+            </div>
+          </section>
+          <section className="Day-item">Day 1</section>
+          <section className="Day-item">Day 2</section>
+          <section className="Day-item">Day 3</section>
+        </section>
+        <footer>footer section</footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
